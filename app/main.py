b8b9be0205afe8a90dcd21671ffc813ad68cb7d8
KRMadhusanka99 <kkrmadhu1999@gmail.com>
2024-04-22 01:06:18 +0530

    post endpoint with pydentic base model

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 #initialize FastAPI
 
 app = FastAPI()
+
+# define a data model for the request body
+
+class Product(BaseModel):
+    name: str
+    price: int
+
 
 @app.get("/ok")
 async def get_ok():
@@ -15,3 +23,7 @@ async def get_name(name: str = 'Ramesh'):
 @app.post("/order")
 async def place_order(product_name:str, price:int):
     return {"message":f"{product_name} is {price} rupees"}
+
+@app.post("/oder_pydentic")
+async def place_order(product: Product):
+    return {"message":f"{product.name} is {product.price} rupees"}
