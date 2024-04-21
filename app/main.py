5aae136da350bf5d521ddadff4209a3ab1d0b119
KRMadhusanka99 <kkrmadhu1999@gmail.com>
2024-04-22 00:34:48 +0530

    post endpoint

--- app/main.py
+++ app/main.py
@@ -6,8 +6,12 @@ app = FastAPI()
 
 @app.get("/ok")
 async def get_ok():
-    return{"message":"hellow world"}
+    return {"message":"hellow world"}
 
 @app.get("/name")
 async def get_name(name: str = 'Ramesh'):
-    return{"message":name}
+    return {"message":name}
+
+@app.post("/order")
+async def place_order(product_name:str, price:int):
+    return {"message":f"{product_name} is {price} rupees"}
